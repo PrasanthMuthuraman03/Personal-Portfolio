4086218d1ea339236c33a82bcece30d900357213
PrasanthMuthuraman03 <prasanth.m@snscecbe.onmicrosoft.com>
2025-05-17 10:59:28 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 This line fixes the white screen issue on Netlify
   plugins: [react(), tailwindcss()],
-  
 });

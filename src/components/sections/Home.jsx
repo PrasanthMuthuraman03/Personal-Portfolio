37cd124da49313ae71acb48f4486bc1bd583e964
PrasanthMuthuraman03 <prasanth.m@snscecbe.onmicrosoft.com>
2025-05-17 12:15:53 +0530

    Resume
    Resume

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -13,11 +13,10 @@ export const Home = () => {
           </h1>
 
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-           I'm a full-stack developer passionate about building fast, clean, and user-focused web applications using React, Node.js, MongoDB, and Express. I love turning ideas into seamless digital experiences.
-
-
+            I'm a full-stack developer passionate about building fast, clean, and user-focused web applications using React, Node.js, MongoDB, and Express. I love turning ideas into seamless digital experiences.
           </p>
-          <div className="flex justify-center space-x-4">
+
+          <div className="flex flex-col sm:flex-row justify-center items-center gap-4 mt-4 sm:space-x-4">
             <a
               href="#projects"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
@@ -31,6 +30,14 @@ export const Home = () => {
              hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
             >
               Contact Me
+            </a>
+
+            <a
+              href="/Prasanth_Resume.pdf"
+              download
+              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(34, 197, 94, 0.4)]"
+            >
+              Download CV
             </a>
           </div>
         </div>
